0391a161c7c061e9a862a2ff66814a8c88e359ef
ShengYi <ShengYi@LAPTOP-0MOT393M>
2023-11-28 14:57:34 +0800

    Add project files.

---
+++ Controllers/Data/Enum/MovieCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace Try.Controllers.Data.Enum
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary,
+        Cartoon,
+        Horror
+    }
+}
---
+++ Models/IEntityBase.cs
@@ -0,0 +1,6 @@
+﻿namespace Try.Models
+{
+    public interface IEntityBase
+    {
+    }
+}
